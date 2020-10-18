60cc93e325bd0f375762b9b5ec3de469ddf806a4
kyle67 <daniels.kyle@gmail.com>
2020-10-18 19:26:46 -0400

    created functions to change background color of piano keys

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ keys.forEach((key)=>{
 })
 
 // Write named functions that change the color of the keys below
+const keyPlay = (event)=>{
+     event.target.style.backgroundColor='green'
+}
 
+const keyReturn = (event)=>{
+    event.target.style.backgroundColor=''
+}
 
 // Write a named function with event handler properties
 
