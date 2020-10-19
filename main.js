03755d3f97d29cc819eebef1abd44a40041a160e
kyle67 <daniels.kyle@gmail.com>
2020-10-18 23:28:12 -0400

    modified event handler for nextThree

--- main.js
+++ main.js
@@ -72,6 +72,13 @@ nextThree.onclick = ()=>{
     document.getElementById('word-four').innerHTML='DAY';
     document.getElementById('word-five').innerHTML='TO';
     document.getElementById('word-six').innerHTML='YOU';
+    document.getElementById('letter-note-one').innerHTML='F';
+    document.getElementById('letter-note-two').innerHTML='F';
+    document.getElementById('letter-note-three').innerHTML='E';
+    document.getElementById('letter-note-four').innerHTML='C';
+    document.getElementById('letter-note-five').innerHTML='D';
+    document.getElementById('letter-note-six').innerHTML='C';
+    lastLyric.style.display='none';
 }
 
 
